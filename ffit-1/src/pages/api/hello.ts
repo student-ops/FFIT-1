7c6bfcb9d8c0f58616fd047a9a279c88c7a2619d
student-ops <gogakuec@gmail.com>
2023-11-21 12:35:29 +0900

    add parse md sample and reset index.tsx

--- ffit-1/src/pages/api/hello.ts
+++ ffit-1/src/pages/api/hello.ts
@@ -1,13 +1,7 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import { NextApiRequest, NextApiResponse } from 'next';
 
-type Data = {
-  name: string
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  await new Promise(resolve => setTimeout(resolve, 5000));
+  res.status(200).send("# hello \n from api");
 }
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
-}
---
+++ ffit-1/src/pages/samples.tsx
@@ -0,0 +1,11 @@
+const Samples: React.FC = () => {
+  return (
+    <main className="h-screen	">
+      <div className="text-blue-400 underline">
+        <a href="/samples/parseMD">parseMD</a>
+      </div>
+    </main>
+  );
+};
+
+export default Samples;
