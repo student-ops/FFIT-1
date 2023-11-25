4e3402db2f68e7890b4a49ce00b616499a11d18f
student-ops <gogakuec@gmail.com>
2023-11-25 13:31:40 +0900

    add openai sdk and it's sample

--- ffit-1/src/server/samples/youtube_subt.ts
+++ ffit-1/src/server/samples/youtube_subt.ts
@@ -4,7 +4,16 @@ async function main() {
     // console.log("hello world")
     const video_id = ""
     const api_addres = "https://www.googleapis.com/youtube/v3"
-    YoutubeTranscript.fetchTranscript('https://www.youtube.com/watch?v=WtVhFs8SJKQ').then(console.log);
+    const response = await YoutubeTranscript.fetchTranscript('https://www.youtube.com/watch?v=-cn3MAovsN4')
+    // console.log(response)
+
+    let whole_text = ""
+    response.forEach((element: { text: string; }) => {
+        console.log(element.text)
+        whole_text += element.text
+    })
+
+
 }
 
 main()
