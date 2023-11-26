ccd053da62b3e6e6f1cae26528fc1eebf4a03242
student-ops <gogakuec@gmail.com>
2023-11-27 07:35:12 +0900

    add url_to_md nad json
    achieved about half of my goal

--- ffit-1/src/server/samples/subtitle_to_md.ts
+++ ffit-1/src/server/samples/subtitle_to_md.ts
@@ -1,17 +1,45 @@
-import { YoutubeTranscript } from 'youtube-transcript';
-import dotenv from 'dotenv';
+import { YoutubeTranscript } from "youtube-transcript";
+import dotenv from "dotenv";
 dotenv.config();
-import OpenAI from 'openai';
+import OpenAI from "openai";
+import fs from "fs";
 
 const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY, // defaults to process.env["OPENAI_API_KEY"]
 });
 
+const jsonObject = {
+    message: "string",
+    tech_article: {
+        title: "string",
+        chapter: [
+            {
+                name: "string",
+                text: "string",
+            },
+        ],
+    },
+};
+
+const jsonObjectMD = {
+    message: "string",
+    tech_article: {
+        title: "string",
+        body: [
+            {
+                MarkdownElement: "string",
+                text: "string",
+            },
+        ],
+    },
+};
 
 async function main() {
     const variableName = ""; // Replace variableName with your desired variable name
 
-    const response = await YoutubeTranscript.fetchTranscript('https://www.youtube.com/watch?v=-cn3MAovsN4');
+    const response = await YoutubeTranscript.fetchTranscript(
+        "https://www.youtube.com/watch?v=R7t7zca8SyM"
+    );
     // console.log(response)
 
     let whole_text = "";
@@ -22,16 +50,80 @@ async function main() {
     console.log(whole_text);
     console.log(whole_text.length.toString());
     // count whole_text word count
-    console.log("subtile num of words " + whole_text.split(" ").length.toString());
-    const stream = await openai.chat.completions.create({
-        model: 'gpt-4-1106-preview',
-        messages: [
-            // { "role": "user", "content": "Please translate the subtitles of the given English video into Japanese 与えられた英語の字幕を日本語に訳して. Then reply as an article described as mark donw. Again, please reply in md format." },
-            { "role": "user", "content": "与えられた英文を日本語に翻訳してください。そして翻訳後のテキスト全文をmarkdonw として返答してください。繰り返しますがmarkdown として記述して" },
-            { "role": "assistant", "content": whole_text }
-        ],
+
+    const json_string = JSON.stringify(jsonObject);
+    console.log(
+        "subtile num of words " + whole_text.split(" ").length.toString()
+    );
+    const messages: OpenAI.Chat.Completions.ChatCompletionMessageParam[] = [
+        {
+            role: "user",
+            content:
+                "与えられた英文を日本語に翻訳してください。そして翻訳後のテキスト全文を技術記事のような形式で返答してください。記事を作成する際には、複数の章を適切に作成してください。また元のテキストの順番はあまり変更しないで。",
+        },
+        {
+            role: "system",
+            content:
+                "You are a helpful assistant designed to output JSON. like this:" +
+                jsonObject +
+                "\n",
+        },
+        { role: "assistant", content: whole_text },
+    ];
+    let stream = await openai.chat.completions.create({
+        model: "gpt-4-1106-preview",
+        response_format: { type: "json_object" },
+        messages: messages
     });
-    console.log(stream.choices[0].message.content);
+    const resp_message: OpenAI.Chat.Completions.ChatCompletionMessageParam[] = [
+        {
+            role: "user",
+            content:
+                "与えられた英文を日本語に翻訳してください。そして翻訳後のテキスト全文を技術記事のような形式で返答してください。記事を作成する際には、複数の章を適切に作成してください。また元のテキストの順番はあまり変更内容にして。",
+        },
+        {
+            role: "system",
+            content:
+                "You are a helpful assistant designed to output JSON. like this:" +
+                jsonObject +
+                "\n",
+        },
+        {
+            role: "user",
+            content: "please continue"
+        }
+    ]
+    console.log("################################");
+    console.log(stream.choices[0].message);
+    console.log("################################");
+    let i = 0;
+
+    while (true) {
+        if (stream.choices[0].finish_reason == "length") {
+            messages.push({ role: "user", content: "please continue" })
+            let stream = await openai.chat.completions.create({
+                model: "gpt-4-1106-preview",
+                response_format: { type: "json_object" },
+                messages: resp_message
+            });
+            console.log("################################");
+            console.log(stream.choices[0].message);
+            console.log("################################");
+            i++;
+            const streamString = JSON.stringify(stream, null, 2);
+            fs.writeFile('stream_output' + i.toString() + '.txt', streamString, (err) => {
+                if (err) {
+                    console.error('ファイルの保存中にエラーが発生しました:', err);
+                } else {
+                    console.log('ファイルが正常に保存されました。');
+                }
+            });
+        }
+        else break;
+
+    }
+
+
 }
 
-main()
+main();
