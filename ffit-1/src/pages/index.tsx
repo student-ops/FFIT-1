5fba10ab6300f25795dbe2ba01a5fdf2ad4f681f
YoshihisaTanaka-prog <72920523+YoshihisaTanaka-prog@users.noreply.github.com>
2023-12-01 18:36:18 +0900

    おかか

--- ffit-1/src/pages/index.tsx
+++ ffit-1/src/pages/index.tsx
@@ -1,13 +1,19 @@
 
 import { Inter } from "next/font/google";
+import { Tab } from "./tab";
+import { MainWindow } from "./main"
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <div id="index-main">
-      <div id="index-tab">test1</div>
-      <div>test2</div>
+      <div id="index-tab">
+        <Tab />
+      </div>
+      <div id="index-window">
+        <MainWindow />
+      </div>
     </div>
   );
 }
