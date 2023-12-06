6c727d94403e1edbf9ba7e584b46ea9f01bd50c5
student-ops <gogakuec@gmail.com>
2023-12-07 03:15:23 +0900

    add loading bar

--- ffit-1/src/components/Index/BodyWrapper.tsx
+++ ffit-1/src/components/Index/BodyWrapper.tsx
@@ -10,13 +10,17 @@ const BodyWrapper: React.FC = () => {
   const [loading, setLoading] = useState<boolean>(false);
   const [markdownContent, setMarkdownContent] = useState<string>("");
   useEffect(() => {
+    let isMounted = true; // 追加
     const fetchMarkdown = async () => {
+      if (!isMounted) return; // 追加
+
       try {
+        setLoading(true);
         const response = await axios.post<{ article: string }>(
           "/api/videoUrl",
           { url: videoUrl }
         );
-        setMarkdownContent(Marked.parse(response.data.article as string));
+        setMarkdownContent(Marked.parse(response.data.article));
       } catch (error) {
         console.error("Error fetching markdown:", error);
         setMarkdownContent("");
@@ -28,27 +32,33 @@ const BodyWrapper: React.FC = () => {
     if (videoUrl) {
       fetchMarkdown();
     }
-  }, [videoUrl]); // videoUrlが変更されたときに実行されます
+
+    return () => {
+      isMounted = false;
+    }; // 追加
+  }, [videoUrl]);
 
   useEffect(() => {
     const videoid = url.match(/v=(.*)/)?.[1];
-    if (url === "test") setVideoId("5C_HPTJg5ek");
-    else setVideoId(videoid || "");
+    if (url === "test") {
+      setVideoId("5C_HPTJg5ek");
+    } else {
+      setVideoId(videoid || "");
+    }
   }, [url]);
 
   const postVideoUrl = async () => {
-    setLoading(true);
-    setVideoUrl(url); // この時点でvideoUrlはまだ更新されていません
-    console.log("url set:", url);
+    if (url === videoUrl) console.log(url + videoUrl);
+    // add same url alert
+    setVideoUrl(url);
   };
-
   return (
     <>
       <div className="flex mx-10">
         <input
           type="text"
           placeholder="URL"
-          className="w-2/3"
+          className="w-11/12"
           value={url}
           onChange={(e) => setUrl(e.target.value)}
           onKeyDown={(e) => {
@@ -62,7 +72,6 @@ const BodyWrapper: React.FC = () => {
         </button>
       </div>
       <p>{url}</p>
-
       <div className="flex mx-10 justify-center bg-white">
         <div className="w-1/2 flex justify-center">
           <div className="w-11/12 py-14">
@@ -74,10 +83,17 @@ const BodyWrapper: React.FC = () => {
         </div>
         <div className="w-1/2 py-12">
           <div style={{ width: "95.8%", marginRight: "4.2%" }}>
-            <div
-              className="markdown"
-              dangerouslySetInnerHTML={{ __html: markdownContent }}
-            />
+            {loading ? (
+              <div className="flex items-center justify-center w-2/3 mx-auto ">
+                <h1 className="">Loding ...</h1>
+                <progress className="progress"></progress>
+              </div>
+            ) : (
+              <div
+                className="markdown"
+                dangerouslySetInnerHTML={{ __html: markdownContent }}
+              />
+            )}
           </div>
         </div>
       </div>
--- ffit-1/src/pages/api/videoUrl.ts
+++ ffit-1/src/pages/api/videoUrl.ts
@@ -4,6 +4,11 @@ import * as fs from 'fs';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     let url = req.body.url;
+    // add sleep 10 sec
+
+    console.log("url is " + url);
+    await new Promise(r => setTimeout(r, 10000));
+
     if (url == "test") {
         try {
             const article = fs.readFileSync("./src/server/assets/rust100.md", "utf-8");
